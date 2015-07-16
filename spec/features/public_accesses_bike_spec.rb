57081b25c85d8b4117995c12dd8d3182eb901c91
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-16 20:48:13 +0100

    refactored

--- spec/features/public_accesses_bike_spec.rb
+++ spec/features/public_accesses_bike_spec.rb
@@ -1,15 +1,14 @@
 require 'capybara/rspec'
 
 describe "public accesses bike" do
+  let(:station) {DockingStation.new}
   scenario 'docking station releases a working bike' do
-    station = DockingStation.new
     station.dock Bike.new
     bike = station.release_bike
     expect(bike).to be_working
   end
 
   scenario 'docking station does not releases bikes when there are none available' do
-    station = DockingStation.new
     expect { station.release_bike }.to raise_error 'No bikes available'
   end
 end
