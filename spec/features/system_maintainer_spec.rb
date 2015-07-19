cde65f0d72b5804d8ac6dbd8d331ad4e832e6fad
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-19 16:31:56 +0100

    changed code to incorporate capacity

--- spec/features/system_maintainer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara/rspec'
-
-describe 'maintainer set up docking station a capacity' do
-  scenario 'capacity set to 20 bikes' do
-    station = DockingStation.new
-    station.capacity = 20
-    expect { station.capacity }.to eq 20
-  end
-end
