e94688e965f64fb9af422ee0ffcc30e88c615e9e
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-20 19:31:49 +0100

    doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -31,8 +31,8 @@ describe DockingStation do
 
   describe 'dock' do
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+      subject.capacity.times { subject.dock double :bike }
+      expect { subject.dock double :bike }.to raise_error "Docking station full"
     end
   end
 
