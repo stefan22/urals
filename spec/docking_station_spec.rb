915908644abfc94846ec1764c2a9853c3788c0dc
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-20 17:50:40 +0100

    does not release broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,6 +21,14 @@ describe DockingStation do
     end
   end
 
+  it 'does not release broken bikes' do
+    bike = Bike.new
+    bike.report_broken
+    subject.dock bike
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+
+
   describe 'dock' do
     it 'raises an error when full' do
       subject.capacity.times { subject.dock Bike.new }
